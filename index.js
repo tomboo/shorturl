7d066987ea59bb18988e46886a6497c468c841f3
tomboo <tom.booster@gmail.com>
2021-08-04 07:22:36 -0700

    Close #6. Add simple API

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express');
+//Import Packages
+import express from "express";
 
+//Import Customs
+import { Storage } from "./src/data/Storage.js";
+import { redirect } from "./src/redirect.js";
+import { cutURL } from "./src/cutURL.js";
+
+//Declarations
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!');
-});
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//Middleware
+app.use(express.json());
+
+//Start Scripts
+if (Storage.data.links === undefined) {
+  data.links = [];
+  write();
+}
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+app.post("/cut", cutURL);
+app.get("/[a-f0-9]{5}", redirect);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
